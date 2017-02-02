04a7a46617c1cc5f2e00494272379a18bb7c4bf5
manojpramesh <manojpramesh@gmail.com>
2017-02-02 20:02:19 +0530

    defined main file
    Identified function to use

---
+++ py-solc.py
@@ -0,0 +1,30 @@
+'''
+Executable file for py-solc compiler
+Execute - python py-solc.py
+'''
+
+from parser import Parser 
+from lexer import Lexer 
+
+def main():
+    # TODO: function to interact with the user
+
+
+def get_args():
+    # TODO: function to set up the argument Parser
+
+
+def to_asm():
+    # TODO: function to compile the given code and give to asm
+
+
+def write_asm():
+    # TODO: function to save the given assembly to disk
+
+
+def assemble_and_link():
+    # TODO: function to assemble and link the file
+
+
+if __name__ == "__main__":
+    main()
